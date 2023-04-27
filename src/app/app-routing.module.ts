11176c87ef7313c14915345ebe1c9f59de5e08b2
tuhin.kole <tuhin.kole@stockedge.com>
2023-04-27 19:09:58 +0530

    add Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { redirectUnauthorizedTo, redirectLoggedInTo, canActivate } from '@angular/fire/auth-guard';
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
+	{
+		path: '',
+		loadChildren: () => import('./login/login.module').then((m) => m.LoginPageModule),
+		...canActivate(redirectLoggedInToHome)
+	},
+	{
+		path: 'home',
+		loadChildren: () => import('./home/home.module').then((m) => m.HomePageModule),
+		...canActivate(redirectUnauthorizedToLogin)
+	},
+	{
+		path: '**',
+		redirectTo: '',
+		pathMatch: 'full'
+	}
 ];
 
 @NgModule({
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,74 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../services/auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+	credentials!:  FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+		private loadingController: LoadingController,
+		private alertController: AlertController,
+		private authService: AuthService,
+		private router: Router
+  ) { }
 
-  ngOnInit() {
+  // Easy access for form fields
+  get email() {
+    return this.credentials.get('email');
   }
 
+  get password() {
+    return this.credentials.get('password');
+  }
+
+  ngOnInit() {
+    this.credentials = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  async register() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+
+    const user = await this.authService.register(this.credentials.value);
+    await loading.dismiss();
+
+    if (user) {
+      this.router.navigateByUrl('/home', { replaceUrl: true });
+    } else {
+      this.showAlert('Registration failed', 'Please try again!');
+    }
+  }
+
+  async login() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+
+    const user = await this.authService.login(this.credentials.value);
+    await loading.dismiss();
+
+    if (user) {
+      this.router.navigateByUrl('/home', { replaceUrl: true });
+    } else {
+      this.showAlert('Login failed', 'Please try again!');
+    }
+  }
+
+  async showAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
 }
