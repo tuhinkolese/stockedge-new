8872a60cc40d33b0de39c666429bde76c51189c5
tuhin.kole <tuhin.kole@stockedge.com>
2023-04-27 19:10:23 +0530

    after successful login go to dashboard

--- src/app/services/avatar.service.ts
+++ src/app/services/avatar.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { doc, docData, Firestore, setDoc } from '@angular/fire/firestore';
+import { getDownloadURL, ref, Storage, uploadString } from '@angular/fire/storage';
+import { Photo } from '@capacitor/camera';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class AvatarService {
+	constructor(private auth: Auth, private firestore: Firestore, private storage: Storage) {}
 
-  constructor() { }
+	getUserProfile() {
+		const user = this.auth.currentUser;
+		const userDocRef = doc(this.firestore, `users/${user?.uid}`);
+		return docData(userDocRef, { idField: 'id' });
+	}
+
+	async uploadImage(cameraFile: any) {
+		const user = this.auth.currentUser;
+		const path = `uploads/${user?.uid}/profile.webp`;
+		const storageRef = ref(this.storage, path);
+
+		try {
+			await uploadString(storageRef, cameraFile.base64String, 'base64');
+
+			const imageUrl = await getDownloadURL(storageRef);
+
+			const userDocRef = doc(this.firestore, `users/${user?.uid}`);
+			await setDoc(userDocRef, {
+				imageUrl
+			});
+			return true;
+		} catch (e) {
+			return null;
+		}
+	}
 }
