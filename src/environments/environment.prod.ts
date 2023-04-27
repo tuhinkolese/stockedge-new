a8ca399e4adbe4b070eb68474e6c3be56d63dad1
tuhin.kole <tuhin.kole@stockedge.com>
2023-04-27 14:05:21 +0530

    firebase added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'stockedge-new',
+    appId: '1:440349205768:web:6749ba2a11267fd0d5f8bd',
+    databaseURL: 'https://stockedge-new-default-rtdb.firebaseio.com',
+    storageBucket: 'stockedge-new.appspot.com',
+    apiKey: 'AIzaSyADdEHQtKNa6k68sFD261uYnHZcf75A4t4',
+    authDomain: 'stockedge-new.firebaseapp.com',
+    messagingSenderId: '440349205768',
+  },
   production: true
 };
